42d6f18cfcc216778940083153f28373031e0a92
Mattiwos B <mattiwosbelachew@gmail.com>
2020-02-04 20:32:45 -0800

    no change

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 package src;
 import src.sort.*;
-
+import java.util.*;
 import java.awt.*;
 
 public class Main{
 
     public static void main(String[] args){
-
-        System.out.println("Working");
+        Bubblesort sortmachine = new Bubblesort();
+        int[] testArray = {1,3,2};
+        
+        System.out.println(java.util.Arrays.toString(sortmachine.sort(testArray)));
     }
 
 
 }
+/**Excute
+ * javac src/*.java; javac src/sort/*.java; java src/Main
+ */
--- src/sort/Bubblesort.java
+++ src/sort/Bubblesort.java
@@ -1,10 +1,30 @@
 package src.sort;
 public class Bubblesort{
+    
     public Bubblesort(){
 
         
     }
-    public void sort(int[] args){
+    public int[] sort(int[] args){
+    int[] x = args;
+    int[] prevx = {};
+    
+    while (prevx != x){
+        prevx = x;
+        for (int i = 0; i < x.length -1; i++){
+            System.out.println(x);
+            if (x[i] > x[i+1]){
+                x[i] = prevx[i+1];
+                x[i+1] = prevx[i];
+                
+                
+            }
+            
+
+        }
+        
+    }
+    return x;
 
     }
 
